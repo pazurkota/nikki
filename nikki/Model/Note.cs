f17a66d3db7da9a2b5eddc3373592f5be7ecb82d
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-04-17 13:14:26 +0200

    Added PreviewText

--- nikki/Model/Note.cs
+++ nikki/Model/Note.cs
@@ -4,4 +4,8 @@ public class Note
 {
     public string Text { get; set; } = string.Empty;
     public DateTime Created { get; set; } = DateTime.Now;
+    
+    public string PreviewText => string.IsNullOrWhiteSpace(Text)
+        ? ""
+        : Text.Length <= 40 ? Text : Text.Substring(0, 40) + "...";
 }
